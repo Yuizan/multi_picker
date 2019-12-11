174688c9d0fdc79c5d7e7fece4be40d85aac2b35
yifan <1205791932@qq.com>
2019-12-11 10:44:49 +0800

    format code

--- lib/multi_picker.dart
+++ lib/multi_picker.dart
@@ -1,17 +1,19 @@
 import 'dart:ui';
 
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:multi_picker/picker_tree.dart';
 
-
 class MultiPicker extends StatefulWidget {
-
-  MultiPicker({Key key, String title, String confirmText, @required this.children, this.onConfirm}) :
-  title = (title == null ? "" : title),
-  confirmText = (confirmText == null ? "Confirm" : confirmText),
-       super(key: key);
+  MultiPicker(
+      {Key key,
+      String title,
+      String confirmText,
+      @required this.children,
+      this.onConfirm})
+      : title = (title == null ? "" : title),
+        confirmText = (confirmText == null ? "Confirm" : confirmText),
+        super(key: key);
 
   final List<List<Map<int, String>>> children;
 
@@ -21,13 +23,11 @@ class MultiPicker extends StatefulWidget {
 
   final void Function(List<Map<int, String>>) onConfirm;
 
-
   @override
   _MultiPickerState createState() => _MultiPickerState();
 }
 
 class _MultiPickerState extends State<MultiPicker> {
-
   List<Map<int, String>> selectedRows = [];
 
   PickerTree pickerTree;
@@ -38,49 +38,51 @@ class _MultiPickerState extends State<MultiPicker> {
     initShowList();
   }
 
-  void initShowList(){
+  void initShowList() {
     pickerTree = new PickerTree();
-    widget.children.forEach((list){
-      list.forEach((node){
+    widget.children.forEach((list) {
+      list.forEach((node) {
         pickerTree.add(node.keys.first, node.values.first);
       });
     });
 
-    if(selectedRows.length == 0 && widget.children.length > 0 && widget.children.first.length > 0){
+    if (selectedRows.length == 0 &&
+        widget.children.length > 0 &&
+        widget.children.first.length > 0) {
       selectedRows.add(widget.children.first.first);
 
       fillSelectedRows();
     }
   }
 
-  void onClickConfirm(){
-    if(widget.onConfirm != null){
+  void onClickConfirm() {
+    if (widget.onConfirm != null) {
       widget.onConfirm(selectedRows);
     }
   }
 
-  void onChangePicker(int position, int index){
-
-    if(index == 0){
-      selectedRows[index] = {pickerTree.root[position].k: pickerTree.root[position].v};
-    }
-    else{
+  void onChangePicker(int position, int index) {
+    if (index == 0) {
+      selectedRows[index] = {
+        pickerTree.root[position].k: pickerTree.root[position].v
+      };
+    } else {
       Map<int, String> topNode = selectedRows[index - 1];
       Node node = pickerTree.get(topNode.keys.first).children[position];
       selectedRows[index] = {node.k: node.v};
     }
 
-    for(int i = index + 1, length = selectedRows.length; i < length;i++){
+    for (int i = index + 1, length = selectedRows.length; i < length; i++) {
       selectedRows.removeAt(index + 1);
     }
     fillSelectedRows();
   }
 
-  void fillSelectedRows(){
-    for(;;){
-      Node node = pickerTree.get( selectedRows.last.keys.first);
-      if(node == null || node.children.length == 0){
-        this.setState((){
+  void fillSelectedRows() {
+    for (;;) {
+      Node node = pickerTree.get(selectedRows.last.keys.first);
+      if (node == null || node.children.length == 0) {
+        this.setState(() {
           selectedRows = selectedRows;
         });
         break;
@@ -88,10 +90,11 @@ class _MultiPickerState extends State<MultiPicker> {
       selectedRows.add({node.children.first.k: node.children.first.v});
     }
   }
-  Iterable<Widget> _renderTitle(int index){
+
+  Iterable<Widget> _renderTitle(int index) {
     Iterable<Widget> widgets = [];
-    if(index == 0){
-      widgets = pickerTree.root.map((node){
+    if (index == 0) {
+      widgets = pickerTree.root.map((node) {
         return Container(
           width: double.maxFinite,
           child: Center(
@@ -99,9 +102,9 @@ class _MultiPickerState extends State<MultiPicker> {
           ),
         );
       });
-    }else{
+    } else {
       Map<int, String> obj = selectedRows[index - 1];
-      widgets = pickerTree.get(obj.keys.first).children.map((node){
+      widgets = pickerTree.get(obj.keys.first).children.map((node) {
         return Container(
           width: double.maxFinite,
           child: Center(
@@ -113,13 +116,14 @@ class _MultiPickerState extends State<MultiPicker> {
     return widgets;
   }
 
-  Iterable<Widget> _renderPicker(){
+  Iterable<Widget> _renderPicker() {
     List<Widget> list = [];
-    for(int i = 0; i < widget.children.length; i++){
+    for (int i = 0; i < widget.children.length; i++) {
       list.add(Expanded(
-        child: CupertinoPicker(itemExtent: 45,
+        child: CupertinoPicker(
+          itemExtent: 45,
           backgroundColor: Colors.white,
-          onSelectedItemChanged: (position){
+          onSelectedItemChanged: (position) {
             onChangePicker(position, i);
           },
           children: _renderTitle(i).toList(),
@@ -137,29 +141,37 @@ class _MultiPickerState extends State<MultiPicker> {
         mainAxisAlignment: MainAxisAlignment.end,
         children: <Widget>[
           Container(
-            padding: EdgeInsets.only(left: 10,right: 10),
+            padding: EdgeInsets.only(left: 10, right: 10),
             height: 50,
             decoration: BoxDecoration(
               color: Colors.white,
               borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(10.0), topRight: Radius.circular(10.0)
-              ),
+                  topLeft: Radius.circular(10.0),
+                  topRight: Radius.circular(10.0)),
               border: new Border.all(color: Colors.white, width: 5),
             ),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: <Widget>[
                 GestureDetector(
-                  onTap: (){Navigator.pop(context);},
-                  child: Icon(Icons.close,size: 24, color: Colors.black54),
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                  child: Icon(Icons.close, size: 24, color: Colors.black54),
                 ),
-                Text(widget.title, style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold)),
+                Text(widget.title,
+                    style:
+                        TextStyle(fontSize: 15, fontWeight: FontWeight.bold)),
                 GestureDetector(
                   onTap: onClickConfirm,
                   child: Container(
                       height: 45,
-                      child: Center(child: Text(widget.confirmText, style: TextStyle(color: Theme.of(context).primaryColor, fontWeight: FontWeight.bold, fontSize: 15)))
-                  ),
+                      child: Center(
+                          child: Text(widget.confirmText,
+                              style: TextStyle(
+                                  color: Theme.of(context).primaryColor,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 15)))),
                 ),
               ],
             ),
@@ -174,7 +186,5 @@ class _MultiPickerState extends State<MultiPicker> {
         ],
       ),
     );
-
   }
 }
-
--- lib/picker_tree.dart
+++ lib/picker_tree.dart
@@ -1,69 +1,68 @@
-class Node<T>{
+class Node<T> {
   int k;
   T v;
   List<Node> children;
 
-  Node(this.k, this.v){
+  Node(this.k, this.v) {
     this.k = k;
     this.v = v;
     this.children = new List<Node>();
   }
-
 }
 
-class PickerTree<T>{
-
+class PickerTree<T> {
   List<Node> root;
 
   PickerTree();
 
-  void add(int k, T v){
-    if(root == null){
+  void add(int k, T v) {
+    if (root == null) {
       root = new List();
       root.add(new Node(k, v));
       return;
     }
 
     _retrieveAndAdd(k, v, root);
-
   }
 
-  _retrieveAndAdd(int k, T v, List<Node> children){
+  _retrieveAndAdd(int k, T v, List<Node> children) {
     bool isFind = false;
-    for(int i = 0; i < children.length; i++){
+    for (int i = 0; i < children.length; i++) {
       Node node = children[i];
-      if(node.k == k){
+      if (node.k == k) {
         node.v = v;
         isFind = true;
         return;
       }
 
-      if(node.k.toString() == k.toString().substring(0, children.first.k.toString().length)){
+      if (node.k.toString() ==
+          k.toString().substring(0, children.first.k.toString().length)) {
         _retrieveAndAdd(k, v, node.children);
         return;
       }
     }
 
-    if(!isFind){
+    if (!isFind) {
       Node newNode = new Node(k, v);
       children.add(newNode);
       return;
     }
   }
 
-  Node get(k){
+  Node get(k) {
     Node node = _retrieve(k, root);
     return node;
   }
 
-  Node _retrieve(int k, List<Node> children){
-    for(int i = 0; i < children.length; i++){
+  Node _retrieve(int k, List<Node> children) {
+    for (int i = 0; i < children.length; i++) {
       Node node = children[i];
-      if(node.k == k){
+      if (node.k == k) {
         return node;
       }
 
-      if(node.k.toString() == k.toString().substring(0, children.first.k.toString().length)){
+      if (node.k.toString() ==
+          k.toString().substring(0, children.first.k.toString().length)) {
         Node n = _retrieve(k, node.children);
         return n;
       }
@@ -71,4 +70,3 @@ class PickerTree<T>{
     return null;
   }
 }
-
